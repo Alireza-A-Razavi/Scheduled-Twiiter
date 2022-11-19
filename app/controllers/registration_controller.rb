68dde1ae96463b0ab07ea16d81d5b0a1dc78bde9
alireza-a-razavi <alirezaalavirazavi@hotmail.com>
2022-11-19 23:18:32 +0330

    Added user sessioned id
    Able to log in as user with sessioned id in the rails - but there is a problem of deleted user ids

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -6,7 +6,8 @@ class RegistrationController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            redirect_to root_path, notice: "Successfully created account"
+            session[:user_id] = @user.id
+            redirect_to root_path, notice: Successfully created account
         else
             render :new
         end
@@ -14,6 +15,9 @@ class RegistrationController < ApplicationController
 
     private
     def user_params
-        params.require(:user)
+        params.require(:user).permit(
+            :email, :username, :password, :password_confirmation,
+            :first_name, :last_name
+        )
     end
 end
