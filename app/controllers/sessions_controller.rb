7f1ffa9483e49e3c1246fce909f9d9b03e5f5ac5
alireza-a-razavi <alirezaalavirazavi@hotmail.com>
2022-11-22 17:15:48 +0330

    passowrd reset - password edit
    new features added such as passowrd edit, password reset and mailer refrence, will add the mailer in future commits

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
+    def new
+    end
+
+    def create
+        user = User.find_by(email: params[:email])
+        # print "this is user authenticity status **** \t " + user.authenticate(params[:password]) + "\n"
+        print "this is request user provided password **** \t " + params[:password] + "\n"
+        if user.authenticate(params[:passowrd])
+            print "User has provided valid passowrd"
+        end
+        if user.present? && user.authenticate(params[:passowrd])
+            session[:user_id] = user.id
+            redirect_to root_path, notice: "Logged in successfully"
+        else
+            flash[:alert] = "Invalid email or passowrd"
+            render :new, status: :uprocessable_entity # renders session/new.html.erb 
+        end
+    end
+
     def destroy
         session[:user_id] = nil 
         redirect_to root_path, notice: "Logged out"
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,20 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "about", to: "about#index" 
 
+  get "password", to: "passowrd#edit"
+  patch "password", to: "passowrd#update"
+
   get "signup", to: "registration#new"
   post "signup", to: "registration#create"
+
+  get "signin", to: "sessions#new"
+  post "signin", to: "sessions#create"
+
+  
   delete "logout", to: "sessions#destroy"
+
+  get "password/reset/", to: "password_resets#new"
+  post "password/reset/", to: "password_resets#create"
   
   root to: "main#index"
 end
